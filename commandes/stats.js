47600c7639798b257004f9135472bd43cf392090
iDrunK65 <nicolassintey1012@gmail.com>
2018-07-31 10:02:43 +0200

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -3,14 +3,15 @@ const botconfig = require("../botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
 
 module.exports.run = async (bot, message, args) => {
-  let totalSeconds = (bot.uptime / 1000);
-  let days = Math.floor(totalSeconds / 86400);
-  let hours = Math.floor(totalSeconds / 3600);
-  totalSeconds %= 3600;
-  let minutes = Math.floor(totalSeconds / 60);
-  let seconds = totalSeconds % 60;
-  let uptime = `${days}D ${hours}H ${minutes}M ${seconds}S`;
-    let botembed = new Discord.RichEmbed()
+  let totalSeconds = (bot.uptime/1000);
+   let days = Math.floor(totalSeconds / 86400);
+   totalSeconds %= 86400;
+   let hours = Math.floor(totalSeconds / 3600);
+   totalSeconds %= 3600;
+   let minutes = Math.floor(totalSeconds / 60);
+   let seconds = totalSeconds % 60;
+   let uptime = `${days}D ${hours}H ${minutes}M ${seconds}S`;
+   let botembed = new Discord.RichEmbed()
     .setTitle("Information du bot")
     .setColor("#15f153")
     .addField(":space_invader: Nom du bot", bot.user.username, true)
