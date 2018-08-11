08e380e041c961a30fd12094171b64480b2073f2
iDrunK65 <nicolassintey1012@gmail.com>
2018-08-11 22:34:05 +0200

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -1,45 +1,46 @@
-const Discord = require("discord.js");
-const botconfig = require("../botconfig.json");
-const bot = new Discord.Client({disableEveryone: true});
+  const Discord = require("discord.js");
+  const botconfig = require("../botconfig.json");
+  const bot = new Discord.Client({disableEveryone: true});
 
-module.exports.run = async (bot, message, args) => {
-  var s = (Math.round(bot.uptime / 1000) % 60)
-  var m = (Math.round(bot.uptime / (1000 * 60)) % 60)
-  var h = (Math.round(bot.uptime / (1000 * 60 * 60)))
-  var days = (Math.round(bot.uptime / (1000 * 60 * 60 * 24)));
-  h = (h < 10)? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
-  let uptime = `${days}D ${h}H ${m}M ${s}S`;
-  let botembed = new Discord.RichEmbed()
-    .setTitle("Information du bot")
-    .setColor("#15f153")
-    .addField(":space_invader: Nom du bot", bot.user.username, true)
-    .addField(":id: ID", bot.user.id, true)
-    .addField(":bust_in_silhouette: Auteur", `${botconfig.author}`, true)
-    .addField(":desktop: Serveurs", `${bot.guilds.size} serveurs`, true)
-    .addField(":dividers: Channel", `${bot.channels.size} channels`, true)
-    .addField(":busts_in_silhouette: Utilisateurs", `${bot.users.size} utilisateurs`,true)
-    .addField('\u200B', '\u200B')
-    .addField(":tools: Version", `${botconfig.version}`,true)
-    .addField(":books: library", `${botconfig.library}`,true)
-    .addField(":stopwatch: Uptime", `${uptime}`,true)
-    .addField(":exclamation:  Prefix", `Préfix => **${botconfig.prefix}**`,false)
-    .setFooter(`Made by ${botconfig.author} | Version ${botconfig.version}`) 
-    .setTimestamp();
+  module.exports.run = async (bot, message, args) => {
+    var s = (Math.round(bot.uptime / 1000) % 60)
+    var m = (Math.round(bot.uptime / (1000 * 60)) % 60)
+    var h = (Math.round(bot.uptime / (1000 * 60 * 60)))
+    var days = (Math.round(bot.uptime / (1000 * 60 * 60 * 24)));
+    h = (h < 10)? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    let uptime = `${days}D ${h}H ${m}M ${s}S`;
 
-    message.channel.send(botembed);
-}
+      let botembed = new Discord.RichEmbed()
+      .setTitle("Information du bot")
+      .setColor("#15f153")
+      .addField(":space_invader: Nom du bot", bot.user.username, true)
+      .addField(":id: ID", bot.user.id, true)
+      .addField(":bust_in_silhouette: Auteur", `${botconfig.author}`, true)
+      .addField(":desktop: Serveurs", `${bot.guilds.size} serveurs`, true)
+      .addField(":dividers: Channel", `${bot.channels.size} channels`, true)
+      .addField(":busts_in_silhouette: Utilisateurs", `${bot.users.size} utilisateurs`,true)
+      .addField('\u200B', '\u200B')
+      .addField(":tools: Version", `${botconfig.version}`,true)
+      .addField(":books: library", `${botconfig.library}`,true)
+      .addField(":stopwatch: Uptime", `${uptime}`,true)
+      .addField(":exclamation:  Prefix", `Préfix => **${botconfig.prefix}**`,false)
+      .setFooter(`Made by ${botconfig.author} | Version ${botconfig.version}`) 
+      .setTimestamp();
 
-module.exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  permLevel: 0
-};
+      message.channel.send(botembed);
+  }
 
-module.exports.help = {
-  name: 'stats',
-  description: 'Affiche toutes les informations du bot.',
-  usage: 'stats',
-};
+  module.exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    permLevel: 0
+  };
+
+  module.exports.help = {
+    name: 'stats',
+    description: 'Affiche toutes les informations du bot.',
+    usage: 'stats',
+  };
 
