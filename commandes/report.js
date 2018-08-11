577db9f1231ae30160b329eee326b8888e7625bd
iDrunK65 <nicolassintey1012@gmail.com>
2018-08-11 22:32:24 +0200

    Update report.js

--- commandes/report.js
+++ commandes/report.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
@@ -7,12 +8,15 @@ module.exports.run = async (bot, message, args) => {
     if(!rreason) return message.channel.send("Il faut un motif !");
 
     let reportEmbed = new Discord.RichEmbed()
-    .setTitle("Signalement") 
+    .setTitle("~ Signalement ~") 
     .setColor("#15f153")
-    .addField("Joueur signalé", `${rUser} (ID: ${rUser.id})`)
-    .addField("Signalé par", `${message.author} (ID: ${message.author.id})`)
-    .addField("Signalé à", message.createdAt)
-    .addField("Motif", rreason);
+    .addField("Joueur signalé", `${rUser}`,true)
+    .addField("Signalé par", `${message.author}`, true)
+    .addField("Signalé à", new Date())
+    .addField("Channel du report", message.channel)
+    .addField("Motif", rreason)
+    .setFooter(`Made by ${botconfig.author} | Version ${botconfig.version}`)
+    .setTimestamp();
 
     let reportschannel = message.guild.channels.find(`name`, "report");
     if(!reportschannel) return message.channel.send("Je ne trouve pas le channel de signalement !");
@@ -20,9 +24,19 @@ module.exports.run = async (bot, message, args) => {
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
+    message.author.send("Ton signalemment a bien été envoyé.")
+    console.log(`[STAFF] ${rUser.user.tag} a été signalé de ${message.guild.name} par ${message.author.tag} pour la raison suivante : ${rreason}.`)
 
 }
  
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  permLevel: 0
+};
+
 module.exports.help = {
-  name: "report"
-}
+  name: 'report',
+  description: 'Permet de signaler une personne du discord.',
+  usage: 'report @pseudo#xxxx motif',
+};
